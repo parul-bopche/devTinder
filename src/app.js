9668d4679d27f8a64924bcc647b12b96a4a3acee
Parul Bopche <parulbopche0420@gmail.com>
2025-07-21 19:34:39 +0530

    exploring get,post, delete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const app = express(); 
 //request handler-make multiple
-app.use("/test",(req,res)=>{
-    res.send("hello from the server!");
+// app.use("/",(req,res)=>{
+//     console.log("namaste parul");
+// });
+// app.get("/hello",(req,res)=>{
+//     res.send("hello!");
+// });//hello/123 same
+// app.get("/test",(req,res)=>{
+//     res.send("hello from the server!");
+// });
+// app.get("/",(req,res)=>{
+//      console.log("namaste parul");
+//  });
+
+app.get("/user",(req,res)=>{
+    res.send({firstName : "parul", lastName:"bopche"});
 });
+
+app.post("/user",(req,res)=>{
+    res.send("save data to the database");
+});
+app.delete("/user",(req,res)=>{
+    res.send("delete data to the database");
+});
+//this will match all thr http method api calls to /test
+app.use("/test",(req,res)=>{
+    res.send("hello from the server");
+});
+
+
+
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000..");
 });
@@ -18,3 +45,9 @@ app.listen(3000,()=>{
 //dependencies
 //what is -g
 //diff btn ^ ans ~
+
+//initialize git
+//.gitignore
+//create a remote repo on github
+//push all  code to remote origin
+//order of routes imp play with routes
